4d49d45d4a8d215755080243c7993f0d943d599c
nathanfranke <nathanfranke@pm.me>
2025-02-25 02:25:46 -0600

    clean up crouch attack logic

--- src/character/state/attack.gd
+++ src/character/state/attack.gd
@@ -8,9 +8,14 @@ extends State
 @onready var timer: Timer = $Timer
 
 
+var _crouch: bool
+
+
 func _enter() -> void:
 	#print("Attack state: ", character.punch, "; ", character.crouch)
 	super._enter()
+	
+	_crouch = character.crouch
 	
 	# Fix same animation not repeating.
 	character.anim.stop()
@@ -18,7 +23,7 @@ func _enter() -> void:
 	
 	if character.punch:
 		hit_timer.start(0.2) # Punch hit time: 200ms
-		if character.crouch:
+		if _crouch:
 			timer.start(0.5) # Punch crouch: 500ms
 			character.anim.play(&"punch_crouch")
 		else:
@@ -26,7 +31,7 @@ func _enter() -> void:
 			character.anim.play(&"punch_standing")
 	else:
 		hit_timer.start(0.3) # Kick hit time: 300ms
-		if character.crouch:
+		if _crouch:
 			timer.start(0.8) # Kick crouch: 800ms
 			character.anim.play(&"kick_crouch")
 		else:
@@ -45,7 +50,7 @@ func _on_hit_timer_timeout() -> void:
 	for body in character.hurt_area.get_overlapping_bodies():
 		if body != character and body is Character:
 			#print(character.name, " try hurt... ", body.name, "; ", character.crouch, ", ", body.crouch)
-			if character.crouch == body.crouch:
+			if _crouch == body.crouch:
 				body.try_damage()
 			break
 
