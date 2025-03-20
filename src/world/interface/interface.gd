827b244c927c3eed1ead8c99dd7088fc03e35355
nathanfranke <nathanfranke@pm.me>
2025-03-20 02:05:07 -0500

    add debug interface text

--- src/world/interface/interface.gd
+++ src/world/interface/interface.gd
@@ -7,8 +7,12 @@ extends CanvasLayer
 @onready var health_bar: ProgressBar = %HealthBar
 @onready var level_name: Label = %LevelName
 
+@onready var debug: Label = %Debug
+
 
 func _ready() -> void:
+	debug.visible = OS.is_debug_build()
+	
 	_update()
 
 
