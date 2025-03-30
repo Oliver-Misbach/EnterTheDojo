9ee681e0c57cb65880cc85289dd0a3b33db8b10c
nathanfranke <nathanfranke@pm.me>
2025-03-30 09:03:13 -0500

    nitpick: move global debug commands to global scene

---
+++ src/global/debug.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func _input(event: InputEvent) -> void:
+	if not OS.is_debug_build():
+		return
+	
+	if event.is_action_pressed(&"debug_reload"):
+		get_tree().reload_current_scene()
+	if event.is_action_pressed(&"debug_pause_resume"):
+		get_tree().paused = not get_tree().paused
--- src/world/debug.gd
+++ src/world/debug.gd
@@ -4,14 +4,10 @@ extends Node
 @export var world: World
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if not OS.is_debug_build():
 		return
 	
-	if event.is_action_pressed(&"debug_reload"):
-		get_tree().reload_current_scene()
-	if event.is_action_pressed(&"debug_pause_resume"):
-		get_tree().paused = not get_tree().paused
 	if event.is_action_pressed(&"debug_next_level"):
 		world.complete_level()
 	if event.is_action_pressed(&"debug_kill_all"):
