abc7c887259d868f452ea221c69616e925e0dd3a
nathanfranke <nathanfranke@pm.me>
2025-03-30 08:16:16 -0500

    finish screen: minor rename

--- src/menu/finish_menu.gd
+++ src/menu/finish_menu.gd
@@ -16,9 +16,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event is InputEventKey:
 		if event.pressed:
-			_on_timer_timeout()
+			finalize_game()
 
 
-func _on_timer_timeout() -> void:
+func finalize_game() -> void:
 	Global.finalize_game()
 	get_tree().change_scene_to_packed(preload("res://src/menu/main_menu.tscn"))
